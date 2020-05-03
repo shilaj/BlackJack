dfd06d458521956a9f90fdc8bccc246196e3b7a6
shilaj <shilajbaral@gmail.com>
2020-05-03 22:54:28 +0545

    added waste logic

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,40 @@
-var images = {
-    1:'./static/images/A.png',
-    2:'./static/images/2.png',
-    3:'./static/images/3.png',
-    4:'./static/images/4.png',
-    5:'./static/images/5.png',
-    6:'./static/images/6.png',
-    7:'./static/images/7.png',
-    8:'./static/images/8.png',
-    9:'./static/images/9.png',
-    10:'./static/images/10.png',
-    // 11:'./static/images/J.png',
-    // 12:'./static/images/Q.png',
-    // 13:'./static/images/K.png'
-
+var blackJackGame = {
+    'you':{'scoreSpan':'#your-score','div':'#your-box','score':0},
+    'dealer':{'scoreSpan':'#dealer-score','div':'#dealer-box','score':0},
+    'cards':['2','3','4','5','6','7','8','9','10','J','Q','K','A'],
+    'cardsMap':{'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'Q':10,'K':10,'A':[1,11]}
 }
-document.querySelector('#hit').addEventListener('click',blackJackHit);
+const YOU = blackJackGame['you'];
+const DEALER = blackJackGame['dealer'];
+
+const hitAudio = new Audio('static/sounds/swish.m4a');
+
+document.querySelector('#hitButton').addEventListener('click',blackJackHit);
+document.querySelector('#standButton').addEventListener('click',blackJackStand);
+document.querySelector('#resetButton').addEventListener('click',resetBox);
 function blackJackHit(){
-    var randomNumber = Math.floor(Math.random()*10+1);
-    var image = document.createElement('img');
-    image.setAttribute('src',images[randomNumber]);
-    image.setAttribute('height',150);
-    image.setAttribute('width',100);
-    document.getElementById('your-box').appendChild(image);
+   showCard(randomCardGenerator(),YOU); 
+}
+function blackJackStand(){
+    showCard(DEALER);
+}
+
+function showCard(card,activePlayer){
+    var cardImage = document.createElement('img');
+    cardImage.src = `static/images/${card}.png`;
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
+    hitAudio.play();
+}
+
+function resetBox(){
+    var selectImage = document.querySelector('.flex-box-container-row1').querySelectorAll('img');
+    for (i=0;i < selectImage.length;i++){
+        selectImage[i].remove();
+    }
 
 }
-// function dealerBox() {
-//         count = 0;
-//         while (count <= 21){
-
-//         }
-// }
+function randomCardGenerator(){
+    var randomNUmber = Math.floor(Math.random()*13);
+    fuckYouBitch = blackJackGame['cards'][randomNUmber];
+    return fuckYouBitch;
+}
